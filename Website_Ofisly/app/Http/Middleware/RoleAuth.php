934c76581190a42460518e97c92c89ce904ae588
Mahayoga <myoga.bahtiar@gmail.com>
2025-08-09 21:25:33 +0700

    Add Templeate SB Admin

---
+++ Website_Ofisly/app/Http/Middleware/RoleAuth.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RoleAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if(!session('is_logged_in')) {
+            // TODO
+            /**
+             * Ini kasih redirect access ditolak atau kode 401
+             */
+        }
+        return $next($request);
+    }
+}
--- Website_Ofisly/routes/web.php
+++ Website_Ofisly/routes/web.php
@@ -5,11 +5,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    return view('admin.dashboard.index');
+})->middleware(['auth', 'verified', 'role.auth'])->name('dashboard');
+
+Route::get('/error_code', function() {
+    abort(401);
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
