ce55ff981ca240a921c48d0452e716a60ebd1311
Mahayoga <myoga.bahtiar@gmail.com>
2025-08-10 14:53:28 +0700

    Cuma halaman biasa

--- Website_Ofisly/resources/views/admin/surat_tugas/index.blade.php
+++ Website_Ofisly/resources/views/admin/surat_tugas/index.blade.php
@@ -1,4 +1,11 @@
 @extends('admin.layout.index')
 @section('content')
-  
+<main>
+  <div class="container-fluid px-4">
+    <h1 class="mt-4">Data Surat Tugas</h1>
+    <ol class="breadcrumb mb-4">
+      <li class="breadcrumb-item active">Kumpulan data surat tugas</li>
+    </ol>
+  </div>
+</main>
 @endsection
