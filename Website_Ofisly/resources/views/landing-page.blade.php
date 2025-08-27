9eecdc07b616fb0ec41fe556d0a505f9c59d462f
Mahayoga <myoga.bahtiar@gmail.com>
2025-08-27 12:26:36 +0700

    Perbaikan conflict

--- Website_Ofisly/resources/views/landing-page.blade.php
+++ Website_Ofisly/resources/views/landing-page.blade.php
@@ -71,50 +71,6 @@
 
     </section><!-- /About Section -->
 
-    <!-- Stats Section -->
-    <section id="stats" class="stats section">
-
-        <div class="container" data-aos="fade-up" data-aos-delay="100">
-
-            <div class="row gy-4">
-
-                <div class="col-lg-3 col-md-6">
-                    <div class="stats-item text-center w-100 h-100">
-                        <span data-purecounter-start="0" data-purecounter-end="3000" data-purecounter-duration="1"
-                            class="purecounter"></span>
-                        <p>Clients</p>
-                    </div>
-                </div><!-- End Stats Item -->
-
-                <div class="col-lg-3 col-md-6">
-                    <div class="stats-item text-center w-100 h-100">
-                        <span data-purecounter-start="0" data-purecounter-end="210" data-purecounter-duration="1"
-                            class="purecounter"></span>
-                        <p>Projects</p>
-                    </div>
-                </div><!-- End Stats Item -->
-
-                <div class="col-lg-3 col-md-6">
-                    <div class="stats-item text-center w-100 h-100">
-                        <span data-purecounter-start="0" data-purecounter-end="12" data-purecounter-duration="1"
-                            class="purecounter"></span>
-                        <p>Hours Of Support</p>
-                    </div>
-                </div><!-- End Stats Item -->
-
-                <div class="col-lg-3 col-md-6">
-                    <div class="stats-item text-center w-100 h-100">
-                        <span data-purecounter-start="0" data-purecounter-end="11" data-purecounter-duration="1"
-                            class="purecounter"></span>
-                        <p>Workers</p>
-                    </div>
-                </div><!-- End Stats Item -->
-
-            </div>
-
-        </div>
-
-    </section><!-- /Stats Section -->
 
     <!-- Services Section -->
     <section id="services" class="services section light-background">
@@ -128,7 +84,7 @@
 
         <div class="container">
             <div class="row gy-4">
-                <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
+                <div class="col-lg-3 col-md-6" data-aos="fade-up" data-aos-delay="100">
                     <div class="service-item position-relative">
                         <div class="icon">
                             <i class="bi bi-bar-chart-line"></i>
@@ -141,7 +97,7 @@
                     </div>
                 </div><!-- End Service Item -->
 
-                <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
+                <div class="col-lg-3 col-md-6" data-aos="fade-up" data-aos-delay="200">
                     <div class="service-item position-relative">
                         <div class="icon">
                             <i class="bi bi-envelope-paper"></i>
@@ -154,7 +110,7 @@
                     </div>
                 </div><!-- End Service Item -->
 
-                <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
+                <div class="col-lg-3 col-md-6" data-aos="fade-up" data-aos-delay="300">
                     <div class="service-item position-relative">
                         <div class="icon">
                             <i class="bi bi-calendar2-week"></i>
@@ -164,6 +120,19 @@
                         </a>
                         <p>Ajukan dan kelola cuti karyawan secara online, lengkap dengan
                             status persetujuan yang transparan.</p>
+                    </div>
+                </div><!-- End Service Item -->
+
+                <div class="col-lg-3 col-md-6" data-aos="fade-up" data-aos-delay="300">
+                    <div class="service-item position-relative">
+                        <div class="icon">
+                            <i class="bi bi-ui-checks"></i>
+                        </div>
+                        <a href="" class="">
+                            <h3>Lowongan Pekerjaan</h3>
+                        </a>
+                        <p>Berbagai macam pilihan lowongan pekerjaan yang menarik,
+                        dan transparan.</p>
                     </div>
                 </div><!-- End Service Item -->
             </div>
